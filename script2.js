b37da4dbbc0ade664f6df5e8d6cb73d825420321
KushalPatidar <kushpatidar1505@gmail.com>
2023-10-15 22:21:02 +0530

    first Commit

---
+++ script2.js
@@ -0,0 +1,13 @@
+let c = document.querySelectorAll('.cloth')
+c[0].addEventListener('click',()=>{
+    window.open("p4.html");
+})
+c[1].addEventListener('click',()=>{
+    window.open("p2.html");
+})
+c[2].addEventListener('click',()=>{
+    window.open("p1_details.html");
+})
+c[3].addEventListener('click',()=>{
+    window.open("p3.html");
+})
